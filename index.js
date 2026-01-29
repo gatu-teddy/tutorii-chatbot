02a0212a2a80b49aa80b83c8740d91bce4340e73
gatu_teddy <59992213+Retard-the-old@users.noreply.github.com>
2026-01-29 18:29:16 +0400

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ const ADMIN_TRIGGER = "Trigger max"     // Replace with your specific admin trig
 // --------------------
 // Target number to send messages to
 // --------------------
-const TARGET_NUMBER = "+254796143065" // Recipient of template & GPT reply
+const TARGET_NUMBER = "+971523534063" // Recipient of template & GPT reply
 
 // --------------------
 // Load prompts
